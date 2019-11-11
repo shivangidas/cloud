43be69cda61a016c46f9b91d657da3c357cf5088
Shivangi <shivangi.das7@gmail.com>
2019-11-11 22:52:07 +0000

    crude version of golden nonce

--- container_app/goldenNonce.py
+++ container_app/goldenNonce.py
@@ -1,10 +1,10 @@
 import hashlib
-import datetime
+# import datetime
 
 from .sellerie import app
 
 
-@app.Nonce
+@app.task
 # def findNonce(start, end):
     # for i in range(start, end):
     #     prefix = "0" * i
@@ -19,22 +19,23 @@ def goldenEgg(Nonce = 0, data ="COMSM0010cloud", prefix= "0000", start=0, end=10
     prefLength = len(prefix)
     flag = 0
     hashValue = ""
-    start = datetime.datetime.now()
+    #startTime = datetime.datetime.now()
     #while flag == 0:
     for i in range(start, end):
         z = str(Nonce) + data #need prev hash and rest of the block
         hashValue = hashlib.sha256(z.encode()).hexdigest()
         if hashValue[:prefLength] == prefix:
             flag = 1
+            break
         Nonce += 1
-    end = datetime.datetime.now()
-    timeSpent = end - start
-    if (flag = 0):
-        return 0, 0, timeSpent
+    #endTime = datetime.datetime.now()
+    #timeSpent = endTime - startTime
+    if (flag == 0):
+        return 0
     else:
         print ("prefix : ", prefix)
         print ("Nonce : ", Nonce)
         print ("Hash Value : ", hashValue)
-        print ("Time spent : ", timeSpent)
+       # print ("Time spent : ", timeSpent)
         print ("\n")
-        return Nonce , hashValue, timeSpent
+        return Nonce
--- container_app/submit_jobs.py
+++ container_app/submit_jobs.py
@@ -1,16 +1,18 @@
 import time
-
+from celery.result import ResultBase
 from container_app.goldenNonce import goldenEgg
 
 if __name__ == '__main__':
     start_time = time.time()
     for i in range(100):
-        GN, hashV, time = goldenEgg.delay(i*100, data, "0000", i*100, i*100 + 100)
-        if GN == 0:
-            break
-    print("golden Nonce: {GN}\n Hash Value: {hashV}")
+        result = goldenEgg.delay(i*1000, "COMSM0010cloud", "0000", i*1000, i*1000 + 1000)
+        print (f'golden nonce {i}: ')
+        print (result.children)
 
-    #result_output = result.wait(timeout=None, interval=0.5)
-    #print(f'Last scheduled task result: {result_output}')
+        
+    #print(f'golden Nonce: {GN}')
+
+    result_output = result.wait(timeout=None, interval=0.5)
+    print(f'Last scheduled task result: {result_output}')
     elapsed_time = time.time() - start_time
     print(f"elapsed time: {elapsed_time}")

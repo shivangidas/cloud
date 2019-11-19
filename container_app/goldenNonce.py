512ae137cad924736ace18ca5d21489cbcf571d8
Shivangi <shivangi.das7@gmail.com>
2019-11-19 20:56:48 +0000

    refractored

--- container_app/goldenNonce.py
+++ container_app/goldenNonce.py
@@ -1,39 +1,20 @@
 import hashlib
-import datetime
 
 from .sellerie import app
-from celery.result import AsyncResult
 
 
 @app.task
 def goldenEgg(Nonce=0, data="COMSM0010cloud", difficulty=4, end=10000):
-    # convert to binary
-    #data = ' '.join(format(ord(x), 'b') for x in data)
-    #print(f"Run {start} {end}")
     prefix = "0" * difficulty
-    prefLength = difficulty
     flag = 0
-    hashValue = ""
-    startTime = datetime.datetime.now()
-    # while flag == 0:
-    # for i in range(start, end+1):
     while Nonce <= end:
         z = str(Nonce) + data
         hashValue = hashlib.sha256(z.encode()).hexdigest()
-        if hashValue[:prefLength] == prefix:
+        if hashValue[:difficulty] == prefix:
             flag = 1
             break
         Nonce += 1
-    endTime = datetime.datetime.now()
-    timeSpent = endTime - startTime
-
     if (flag == 0):
         return 0
-        # print("Not found Nonce")
     else:
-        # print("prefix : ", prefix)
-        # print("Nonce : ", Nonce)
-        # print("Hash Value : ", hashValue)
-        # print("Time spent : ", timeSpent)
-        # print("\n")
         return Nonce
